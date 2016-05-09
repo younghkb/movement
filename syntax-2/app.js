07005264cfcf3676e1e90a2f910387a81d70ce1a
Hannah Young <hyoung@hmc.edu>
2016-05-09 12:52:21 -0700

    made ui changes and ccode cleanup

--- syntax-2/app.js
+++ syntax-2/app.js
@@ -1,38 +1,60 @@
 /*
- * Add items to a list - from cssanimation.rocks/list-items/
+ * Hannah Young
+ * DSL's, Spring 2016
+ * Controlling the syntax and grabbing values
+ * The IR Level
  */
-var counter = 1;
 
+// Global counter to track each movement
+var counter = 2;
+
+
+/*
+ * Anonymous fn that generates the list items where users 
+ * can record a new movement in a phrase
+ * Reference- from cssanimation.rocks/list-items/
+ */
 document.getElementById('add-to-list').onclick = function() {
   var list = document.getElementById('list');
   var newLI = document.createElement('li');
-  newLI.innerHTML = '<form action="#">'
-              + '<p>'
-                +  '<label class="label">Movement No. ' + counter + ':     </label>'
-                +  '<select class="turnintodropdown">'
-                   +   '<option>Please select a level:</option>'
-                      +    '<option>High</option>'
-                      +    '<option>Medium</option>'
-                       +   '<option>Deep</option>'
+  newLI.innerHTML = '<form action="#" id="' + counter +'">'
+                  + '<p>'
+                  + '<label class="label">Movement No. ' + counter + ':</label>'
+                  + '<div class="instruction"> Vertical: </div>'
+                  + '<select class="field">'
+                  + '<option value="high">High</option>'
+                  + '<option value="medium">Medium</option>'
+                  + '<option value="deep">Deep</option>'
                   + '</select>'
-                +  '<select class="turnintodropdown">'
-                  +    '<option>Please select a lateral direction:</option>'
-                        +  '<option>Left</option>'
-                        +  '<option>Right</option>'
-                       +   '<option>None</option>'
-                 +  '</select>'
-                +   '<select class="turnintodropdown">'
-                  +    '<option>Please select a sagittal direction:</option>'
-                      +    '<option>Forward</option>'
-                       +   '<option>Back</option>'
-                       +   '<option>None</option>'
-                 +  '</select>'
-           +   '</p>'
-           + ' Time duration (sec): <input name="username" type="text" class="time"> '
-       +   '</form>';
+                  + '<div class="instruction"> Lateral: </div>'
+                  + '<select class="field">'
+                  + '<option value="left">Left</option>'
+                  + '<option value="right">Right</option>'
+                  + '<option value="none">None</option>'
+                  + '</select>'
+                  + '<div class="instruction"> Sagittal: </div>'
+                  + '<select class="field">'
+                  + '<option value="forward">Forward</option>'
+                  + '<option value="back">Back</option>'
+                  + '<option value="none">None</option>'
+                  + '</select>'
+                  + '</p>'
+                  + ' <div class="instruction"> Time (sec): </div>'
+                  + '<input name="username" type="text" class="field" placeholder="3">'
+                  + '</form>';
   list.appendChild(newLI);
   counter++;
   setTimeout(function() {
     newLI.className = newLI.className + " show";
   }, 10);
 }
+
+
+/*
+ * createPhrase 
+ * Function that will grab the user's entire movement phrase
+ * If movement phrase contains invalid values, returns errors
+ */
+
+
+
